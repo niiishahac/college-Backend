712e8b5b395ffa92099b8361373e36d02cc00905
niiishahac <ranjusuman1462@gmail.com>
2026-02-16 15:02:06 +0530

    commit

---
+++ day3/index.js
@@ -0,0 +1,21 @@
+import fs from "fs/promises";
+
+try {
+    // Write file
+    await fs.writeFile("test.txt", "Hello");
+    console.log("File written!");
+
+    // Read file
+    const data = await fs.readFile("test.txt", "utf-8");
+    console.log("File content:", data);
+
+    // Remove directory
+    await fs.rm("myFolder", { recursive: true });
+    console.log("Directory removed!");
+
+} catch (error) {
+    console.log("Error:", error.message);
+}
+// comment
+// 
+// xyz
