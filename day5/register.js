2b9757a462a88c16dbde85907e9d0314beb2762a
niiishahac <ranjusuman1462@gmail.com>
2026-02-23 15:13:53 +0530

    day5

---
+++ day5/register.js
@@ -0,0 +1,23 @@
+import { readFile } from "../utils/readAndwrite.js";
+import { writeFile } from "../utils/readAndwrite.js";
+
+const userRegister = async(userDetails) => {
+    const { name, email, password, gender } = userDetails;
+    if (!name || !email || !password || !gender) {
+        console.log("Please provide all the required details");
+        return;
+    }
+
+    const users = await readFile(FILE);
+    if (users.length == 0) {
+        register();
+
+    }
+    const existinguser = users.filter(user => user.email == email);
+    if (existinguser.length === 0) {
+        console.log("User already registered");
+        return;
+    }
+
+    register();
+}
