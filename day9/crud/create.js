269f352daf11b858a454912b8bcac93353dbebb6
niiishahac <ranjusuman1462@gmail.com>
2026-04-13 15:10:21 +0530

    day9

--- day9/crud/create.js
+++ day9/crud/create.js
@@ -28,14 +28,61 @@ const insertMany = async() => {
 insertMany();
 
 const insertOne = async() => {
-        try {
-            const db = await dbConnect();
-            const userLoginCollection = db.collection("login");
-            const user = await userLoginCollection
-                .insertOne({ name: "abc", email: "abc@gmail.com", password: "12345", gender: "F" });
-            console.log("User is created successfully: ", user.insertedId);
-        } catch (error) {
-            console.log("Insertion Error", error.message);
-        }
+    try {
+        const db = await dbConnect();
+        const userLoginCollection = db.collection("login");
+        const user = await userLoginCollection
+            .insertOne({ name: "abc", email: "abc@gmail.com", password: "12345", gender: "F" });
+        console.log("User is created successfully: ", user.insertedId);
+    } catch (error) {
+        console.log("Insertion Error", error.message);
     }
-    // insertOne();
+}
+insertOne();
+const updateOneUser = async() => {
+    try {
+        const db = await dbConnect();
+        const userLoginCollection = db.collection("login");
+
+        const result = await userLoginCollection.updateOne({ email: "abc@gmail.com" }, // filter
+            { $set: { password: "99999" } } // update
+        );
+
+        console.log("Updated One:", result.modifiedCount);
+    } catch (error) {
+        console.log("Update Error", error.message);
+    }
+};
+
+updateOneUser();
+const updateManyUsers = async() => {
+    try {
+        const db = await dbConnect();
+        const userLoginCollection = db.collection("login");
+
+        const result = await userLoginCollection.updateMany({ password: "12145678" }, // filter
+            { $set: { password: "00000" } } // update
+        );
+
+        console.log("Updated Many:", result.modifiedCount);
+    } catch (error) {
+        console.log("Update Error", error.message);
+    }
+};
+
+updateManyUsers();
+const deleteOneUser = async() => {
+    try {
+        const db = await dbConnect();
+        const userLoginCollection = db.collection("login");
+
+        const result = await userLoginCollection.deleteOne({ email: "abc@gmail.com" } // filter
+        );
+
+        console.log("Deleted One:", result.deletedCount);
+    } catch (error) {
+        console.log("Delete Error", error.message);
+    }
+};
+
+deleteOneUser();
